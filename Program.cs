7b7ba7e9a81d4b76508de123da0a98e44bd809cf
michael japaridze <mishojaparidze@gmail.com>
2022-04-27 02:13:15 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HomeWork3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Mikheil Japaridze");
+            Console.WriteLine("Input:");
+            Console.WriteLine(Console.ReadLine()) ;
+        }
+    }
+}
